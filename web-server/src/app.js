b1eb78d8d8b8f0bbc60a41cdfbecf824fd4cf339
mefissto <mefissto.w@gmail.com>
2020-02-06 21:58:34 +0200

    web server

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,11 +1,16 @@
+const path = require('path');
 const express = require('express');
 
-const app = express()
+const app = express();
+const publicDirectoryPath = path.join(__dirname, '../public');
 
-app.get('', (req, res) => {
-    res.send('hello');
+
+app.use(express.static(publicDirectoryPath));
+
+app.get('/weather', (req, res) => {
+  res.send('weater');
 });
 
 app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
-})
+  console.log('Server is up on port 3000.');
+});
